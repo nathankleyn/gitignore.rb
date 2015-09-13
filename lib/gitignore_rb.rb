ec1687830101266ff10ee2d44b48088a82d0d193
Nathan Kleyn <nathan@nathankleyn.com>
2015-09-13 22:18:40 +0100

    Document `GitIgnoreRb` main class.

--- lib/gitignore_rb.rb
+++ lib/gitignore_rb.rb
@@ -1,5 +1,11 @@
 require 'gitignore_rb/rust'
 
+# Represents a `.gitignore` file and the operations one can perform on or with
+# them.
+#
+# This class will take the `.gitignore` file given, parse the rules contained
+# within it, and provides a set of functions to apply these rules in various
+# useful ways.
 class GitIgnoreRb
   def initialize(path)
     @path = path
